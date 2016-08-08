ecfe4cfab1eb6f2c0b6d478168a815e5e99442a9
Sevgin Mustafov <sevgin95@gmail.com>
2016-08-08 10:39:15 +0300

    test

--- app/User.php
+++ app/User.php
@@ -11,6 +11,7 @@ class User extends Authenticatable
      *
      * @var array
      */
+	 
     protected $fillable = [
         'first_name', 'last_name', 'name', 'email', 'password', 'avatar' 
     ];
