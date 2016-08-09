fa757b117316c3b17e14dce092793eba3bdb1436
Sevgin Mustafov <sevgin95@gmail.com>
2016-08-09 21:42:11 +0300

    Show events page implemented

--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -1,41 +1,45 @@
 @extends('layouts.app')
 @section('content')
-
-<div class="container">
+    <link rel="stylesheet" href="/css/custom/showevents.css">
     @if(count($events) > 0)
         <ul class="collection">
             @foreach($events as $event)
-                <h3>{{ $event->title }}</h3>
-                <p>{{ $event->description }}</p>
-                <div>{{ $event->date === '' ? '' : 'Date of event: ' . $event->date }}</div>
-                <div>{{ $event->dress_code === '' ? '' : '(we can load the picture)Dress code: ' . $event->dress_code }}</div>
-                <div>{{ $event->music === '' ? '' : '(we can load the picture)Music: ' . $event->music }}</div>
+                <div class="row row-hoverable">
+                    <div class="col s12 m7">
+                        <div class="card">
+                            <div class="card-image">
+                                @if($event->type === 'Wedding')
+                                    <img src="images/create-event/type/wedding.jpg">
+                                @endif
+                                @if($event->type === 'Conference')
+                                    <img src="images/create-event/type/conference.jpg">
+                                @endif
+                                @if($event->type === 'Bachelor-party')
+                                    <img src="images/create-event/type/bachelor-party.jpg">
+                                @endif
+                                @if($event->type === 'Birthday-party')
+                                    <img src="images/create-event/type/birthday-party.jpg">
+                                @endif
+                                @if($event->type === 'Buisiness-meeting')
+                                    <img src="images/create-event/type/buisiness-meeting.jpg">
+                                @endif
+                                @if($event->type === 'Camp')
+                                    <img src="images/create-event/type/camp.jpg">
+                                @endif
+                                @if($event->type === 'Other')
+                                    <img src="images/create-event/type/other.jpg">
+                                @endif
+                                <span class="card-title">{{ $event->title }}</span>
+                            </div>
 
-                @if(is_array($event->food))
-                    <ul class="collection">
-                        <li class="collection-header"><h4>Food: </h4></li>
-                        @foreach($event->food as $food_item)
-                            <li class="collection-item">{{ $food_item }}</li>
-                        @endforeach
-                    </ul>
-                @endif
-
-                @if(is_array($event->drinks))
-                    <ul class="collection">
-                        <li class="collection-header"><h4>Drinks: </h4></li>
-                        @foreach($event->drinks as $drinks_item)
-                            <li class="collection-item">{{ $drinks_item }}</li>
-                        @endforeach
-                    </ul>
-                @endif
-
-                <div>{{ $event->location === '' ? '' : 'Location: ' . $event->location_string }}</div>
-                <hr>
-                <hr>
-                <hr>
+                            <div class="card-content">
+                                <p>{{ $event->description }}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             @endforeach
         </ul>
     @endif
-</div>
 
 @endsection
