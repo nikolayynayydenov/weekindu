0bddd980acc096ebc84ef9df0734e05efd46671c
Sevgin Mustafov <sevgin95@gmail.com>
2016-08-10 15:18:04 +0300

    Linked events to their links

--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -4,30 +4,44 @@
     @if(count($events) > 0)
         <ul class="collection">
             @foreach($events as $event)
-                <div class="row row-hoverable">
+                <div class="row">
                     <div class="col s12 m7">
-                        <div class="card">
+                        <div class="card hoverable">
                             <div class="card-image">
                                 @if($event->type === 'Wedding')
-                                    <img src="images/create-event/type/wedding.jpg">
+                                    <a href="event/{{ $event->id }}">
+                                    <img class="visible-md visible-xs" src="images/create-event/type/wedding.jpg">
+                                    </a>
                                 @endif
                                 @if($event->type === 'Conference')
+                                    <a href="event/{{ $event->id }}">
                                     <img src="images/create-event/type/conference.jpg">
+                                    </a>
                                 @endif
                                 @if($event->type === 'Bachelor-party')
+                                    <a href="event/{{ $event->id }}">
                                     <img src="images/create-event/type/bachelor-party.jpg">
+                                    </a>
                                 @endif
                                 @if($event->type === 'Birthday-party')
+                                    <a href="event/{{ $event->id }}">
                                     <img src="images/create-event/type/birthday-party.jpg">
+                                    </a>
                                 @endif
                                 @if($event->type === 'Buisiness-meeting')
+                                    <a href="event/{{ $event->id }}">
                                     <img src="images/create-event/type/buisiness-meeting.jpg">
+                                    </a>
                                 @endif
                                 @if($event->type === 'Camp')
+                                    <a href="event/{{ $event->id }}">
                                     <img src="images/create-event/type/camp.jpg">
+                                    </a>
                                 @endif
                                 @if($event->type === 'Other')
+                                    <a href="event/{{ $event->id }}">
                                     <img src="images/create-event/type/other.jpg">
+                                    </a>
                                 @endif
                                 <span class="card-title">{{ $event->title }}</span>
                             </div>
