fa6d6ac1ad3abe919092de40a90dc38861e0a7e2
YovchoKalev <yovchokalev@gmail.com>
2016-08-20 00:42:35 +0300

    made some view fully responsible

--- resources/views/about-us.blade.php
+++ resources/views/about-us.blade.php
@@ -9,9 +9,9 @@
 <body>
 <div class="container">
 <h1 class="orange-text center-align">Meet the team</h1>
-    <div class="row">
-    <div class="col m4 s12 center-align margin-bottom-20">
-        <img class="materialboxed circle" width="400" src="/images/about-us/yovchobe.jpg">
+    <div class="row center">
+    <div class="col m4 s12 l4 center-align">
+        <img class="materialboxed circle responsive-img" width="400" src="/images/about-us/yovchobe.jpg">
         <div class="center">
             <strong style="font-size: 30px">Yovcho Kalev</strong><br>
             <strong style="font-size: 15px">Design,Front-end</strong><br>
@@ -24,11 +24,11 @@
         </div>
 
     </div>
-    <div class="col m4 s12 center-align margin-bottom-20">
-        <img class="materialboxed circle" width="400" src="/images/about-us/niki.jpg">
+    <div class="col m4 s12 l4 ">
+        <img class="materialboxed circle responsive-img" width="400" src="/images/about-us/niki.jpg">
         <div class="center">
             <strong style="font-size: 30px">Nikolay Naydenov</strong><br>
-            <strong style="font-size: 15px">Design,Front-end</strong><br>
+            <strong style="font-size: 15px">Full-stack </strong><br>
             <a href="https://facebook.com/profile.php?id=100009002676266">
                 <img src="/images/icons/facebook.png"/></a>
             <a href="https://github.com/nikolayynayydenov">
@@ -37,11 +37,11 @@
                 <img src="/images/icons/linkedin.png"/></a>
         </div>
     </div>
-    <div class="col m4 s12 center-align margin-bottom-20">
-        <img class="materialboxed circle" width="400" src="/images/about-us/sevgito.jpg">
+    <div class="col m4 s12 l4 ">
+        <img class="materialboxed circle responsive-img" width="400" src="/images/about-us/sevgito.jpg">
         <div class="center">
             <strong style="font-size: 30px">Sevgin Mustafov</strong><br>
-            <strong style="font-size: 15px">Design,Front-end</strong><br>
+            <strong style="font-size: 15px">Back-end</strong><br>
             <a href="https://www.facebook.com/Sevgin.Mustafov.1">
                 <img src="/images/icons/facebook.png"/></a>
             <a href="https://github.com/sMustafov">
