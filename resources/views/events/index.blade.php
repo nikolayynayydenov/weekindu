f380fa01edfb7493401a2aa094c43d59d2452b74
nikolay <nikolayynayydenov@gmail.com>
2016-08-22 15:16:13 +0300

    commit

--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -2,8 +2,8 @@
 @section('content')
     <link rel="stylesheet" href="/css/custom/showevents.css">
     <!-- Modal Trigger -->
-    <a class="waves-effect waves-light btn modal-trigger modal-trigger-desktop accent-color hide-on-med-and-down" href="#modal1">What is this?</a>
-    <a class="waves-effect waves-light btn modal-trigger modal-trigger-mobile accent-color show-on-medium-and-down s12 hide-on-med-and-up" href="#modal1">What is this?</a>
+    <a class="waves-effect waves-light btn modal-trigger modal-trigger-desktop orange hide-on-med-and-down" href="#modal1">What is this?</a>
+    <a class="waves-effect waves-light btn modal-trigger modal-trigger-mobile orange show-on-medium-and-down s12 hide-on-med-and-up" href="#modal1">What is this?</a>
 
     <!-- Modal Structure -->
     <div id="modal1" class="modal">
@@ -16,73 +16,87 @@
         </div>
     </div>
     <div class="container">
-    @if(count($events) > 0)
+        <div class="row">
+            <div class="col s12">
+                <ul class="tabs">
+                    <li class="tab col s3 blue-text"><a href="#all">All Events</a></li>
+                    <li class="tab col s3 blue-text"><a href="#my">My Events</a></li>
+
+                </ul>
+            </div>
+            <div id="all" class="col s12">
+                @if(count($events) > 0)
 
 
-            @foreach($events as $event)
+                    @foreach($events as $event)
 
-                    <div class="col sl2 m5">
-                        <div class="card hoverable center-on-small-only">
-                            <a href="event/{{ $event->id }}">
-                                <div class="card-image">
-                                    @if($event->type === 'Wedding')
-                                        <img class="responsive-img" src="images/create-event/type/wedding.jpg">
-                                    @endif
-                                    @if($event->type === 'Conference')
-                                        <img src="images/create-event/type/conference.jpg">
-                                    @endif
-                                    @if($event->type === 'Bachelor Party')
-                                        <img src="images/create-event/type/bachelor-party.jpg">
-                                    @endif
-                                    @if($event->type === 'Birthday Party')
-                                        <img src="images/create-event/type/birthday-party.jpg">
-                                    @endif
-                                    @if($event->type === 'Buisiness Meeting')
-                                        <img src="images/create-event/type/buisiness-meeting.jpg">
-                                    @endif
-                                    @if($event->type === 'Camp')
-                                        <img src="images/create-event/type/camp.jpg">
-                                    @endif
-                                    @if($event->type === 'Other')
-                                        <img src="images/create-event/type/other.jpg">
-                                    @endif
+                        <div class="col sl2 m12">
+                            <div class="card hoverable center-on-small-only">
+                                <a href="event/{{ $event->id }}">
+                                    <div class="card-image">
+                                        @if($event->type === 'Wedding')
+                                            <img class="responsive-img" src="images/create-event/type/wedding.jpg">
+                                        @endif
+                                        @if($event->type === 'Conference')
+                                            <img src="images/create-event/type/conference.jpg">
+                                        @endif
+                                        @if($event->type === 'Bachelor Party')
+                                            <img src="images/create-event/type/bachelor-party.jpg">
+                                        @endif
+                                        @if($event->type === 'Birthday Party')
+                                            <img src="images/create-event/type/birthday-party.jpg">
+                                        @endif
+                                        @if($event->type === 'Buisiness Meeting')
+                                            <img src="images/create-event/type/buisiness-meeting.jpg">
+                                        @endif
+                                        @if($event->type === 'Camp')
+                                            <img src="images/create-event/type/camp.jpg">
+                                        @endif
+                                        @if($event->type === 'Other')
+                                            <img src="images/create-event/type/other.jpg">
+                                        @endif
 
-                                </div>
-                            </a>
-                            <div class="card-content">
-                                <div class="row">
-                                <p class="center">
-                                    <strong class="center orange-text">{{ $event->title }}</strong><br>
-                                    <div class="divider"></div>
-                                </div>
+                                    </div>
+                                </a>
+                                <div class="card-content">
+                                    <div class="row">
+                                        <p class="center">
+                                            <strong class="center blue-text">{{ $event->title }}</strong><br>
+                                        <div class="divider"></div>
+                                    </div>
 
                                     <div class="row">
                                         <div class="col s10 m10 ">
-                                <a href="{{ url('/event/'.$event->id) }}" class="btn default-primary-color waves-effect manage-button">Manage</a>
-                                        {{--</div>
-                                        <div class="col">--}}
-                                            <a href="{{ url('/invitation/'.$event->invitation_code) }}" class="btn accent-color waves-effect invitation-button">invitation</a>
+                                            <a href="{{ url('/event/'.$event->id) }}" class="btn  waves-effect manage-button white-text">Manage</a>
+                                            {{--</div>
+                                            <div class="col">--}}
+                                            <a href="{{ url('/invitation/'.$event->invitation_code) }}" class="btn waves-effect invitation-button">invitation</a>
                                         </div>
                                         <div class="col right">
                                             <form action="{{url('/event/'.$event->id )}}" method="post">
 
-                                    {{ method_field('delete') }}
-                                    {{ csrf_field() }}
-                                    <button type="submit" class="deletebut " value=>
-                                        <i class="material-icons">delete</i></button>
+                                                {{ method_field('delete') }}
+                                                {{ csrf_field() }}
+                                                <button type="submit" class="deletebut " value=>
+                                                    <i class="material-icons">delete</i></button>
 
-                                </form>
+                                            </form>
                                         </div>
                                     </div>
-                                </p>
+                                    </p>
 
+                                </div>
                             </div>
                         </div>
-                    </div>
 
-            @endforeach
+                    @endforeach
             </div>
 
-    @endif
+            @endif
+        </div>
+        <div id="my" class="col s12">moite</div>
+
+    </div>
+
 
 @endsection
